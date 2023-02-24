9fbef324750977c8b51d34cc4209cd787f07cd35
Walter Ozmore <walter.ozmore.327@gmail.com>
2023-02-24 00:08:30 -0600

    Created console page

---
+++ omegaball/console.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Omegaball || Console</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
+
+    <link rel="stylesheet" href="/omegaball/res/master.css">
+  </head>
+
+  <header>
+    <?php require realpath($_SERVER["DOCUMENT_ROOT"])."/omegaball/res/header.php"; ?>
+  </header>
+
+  <body>
+    Console
+  </body>
+</html>
