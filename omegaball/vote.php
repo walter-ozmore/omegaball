db957f918535c26ded99565f898c7e8d5b8722d4
Austen Somerville <austensomerville@gmail.com>
2023-03-03 16:29:48 -0600

    Added JavaScript to vote.php to make notifications

--- omegaball/vote.php
+++ omegaball/vote.php
@@ -32,10 +32,75 @@
       }
     </style>
     <script>
-      function notify() {
+      function purchase() {
         let temp = createNotification();
-        let ele = mkEle("p", "hi");
+        let ele = mkEle("p", "PURCHASE VOTES:");
         temp.appendChild(ele);
+        let line = mkEle("p", "ENTER THE NUMBER OF VOTES YOU WOULD <br>");
+        line.style.display = "inline";
+        temp.appendChild(line);
+        let line2 = mkEle("p", "LIKE TO PURCHASE: <br>");
+        line2.style.display = "inline";
+        temp.appendChild(line2);
+        let buy = mkEle("p", "(1 VOTE COSTS 10 COINS)");
+        buy.style.color = "gray";
+        temp.appendChild(buy);
+
+        let x = document.createElement("INPUT");
+        x.setAttribute("type", "number");
+        x.setAttribute("min", "1");
+        x.style.display = "block";
+        x.style.marginLeft = "auto";
+        x.style.marginRight = "auto";
+        x.style.marginBottom = "1em";
+        temp.appendChild(x);
+
+        let can = mkEle("button", "CANCEL");
+        can.style.display = "inline";
+        can.style.marginRight = "5em";
+        temp.appendChild(can);
+        can.onclick = function() {
+          closeNotification(this);
+        };
+
+        let cont = mkEle("button", "CONTINUE");
+        cont.style.display = "inline";
+        temp.appendChild(cont);
+      }
+      function submit() {
+        let temp = createNotification();
+        let ele = mkEle("p", "SUBMIT VOTES:");
+        temp.appendChild(ele);
+        let line = mkEle("p", "ENTER THE NUMBER OF VOTES YOU WOULD <br>");
+        line.style.display = "inline";
+        temp.appendChild(line);
+        let line2 = mkEle("p", "LIKE TO SUBMIT: <br>");
+        line2.style.display = "inline";
+        temp.appendChild(line2);
+        let buy = mkEle("p", "(THESE VOTES WILL BE CAST FOR THE SELECTED EDICT)");
+        buy.style.color = "gray";
+        temp.appendChild(buy);
+
+        let x = document.createElement("INPUT");
+        x.setAttribute("type", "number");
+        x.setAttribute("min", "1");
+        x.style.display = "block";
+        x.style.marginLeft = "auto";
+        x.style.marginRight = "auto";
+        x.style.marginBottom = "1em";
+        temp.appendChild(x);
+
+        let can = mkEle("button", "CANCEL");
+        can.style.display = "inline";
+        can.style.marginRight = "5em";
+        temp.appendChild(can);
+        can.onclick = function() {
+          closeNotification(this);
+        };
+
+        let cont = mkEle("button", "CONTINUE");
+        cont.style.display = "inline";
+        temp.appendChild(cont);
       }
     </script>
   </head>
@@ -46,12 +111,14 @@
     ?>
   </header>
 
-  <body onload="notify()">
+  <body>
   <div class="grid-container">
       <!-- Directory -->
       <div id="directory" class="border">
-      <p>  
+      <p> 
+        <button class = "selected"> 
         EDICTS
+        </button>
       </p>
       <p>  
         REWARDS
@@ -69,21 +136,21 @@
           <div> <p>SINKING SHIP</p> </div>
           <div><img class="vote-image" src="https://harpersbazaar.com.au/wp-content/uploads/2022/11/jack-titanic.jpg" alt="Sinking Ship"></div> 
           <div><p>The team with the fewest wins at the end of each season will be eliminated and replaced.</p></div>
-          <div><button>VOTE</button></div>
+          <div><button onclick = "submit()">VOTE</button></div>
 
           <div> <p>FORBIDDEN KNOWLEDGE</p> </div>
           <div><img class="vote-image" src="https://external-preview.redd.it/NQKtqcpXKQYcIRmOg3CJkUHkMvTnppv1hyw6sgDfldQ.jpg?auto=webp&s=724507cbe8c31b90731978f55d275968aad47048" alt="Sinking Ship"></div>
           <div><p>Forbidden.</p></div>
-          <div><button>VOTE</button></div>
+          <div><button onclick = "submit()">VOTE</button></div>
           
           <div><p>SCOREBOARD</p></div>
           <div><img class="vote-image" src="https://ih1.redbubble.net/image.179407712.0276/st,small,507x507-pad,600x600,f8f8f8.u4.jpg" alt="Sinking Ship"></div>
           <div><p>Games will be decided based on a point-scoring system.</p></div>
-          <div><button>VOTE</button></div>
+          <div><button onclick = "submit()">VOTE</button></div>
         </div>
-        <button>
+        <button onclick = "purchase()">
             PURCHASE VOTES
-          </button>
+        </button>
           </center>
       </div>
     </div>
