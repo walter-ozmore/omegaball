8e0bb20442412080c89cb26d12984cf17592c9ba
Taylor Burt <tburt@leomail.tamuc.edu>
2023-04-14 17:54:37 -0500

    Created a profanity.txt file. Called the file to be appended to a variable. Added comments to add-name.php and corrected errors.

--- omegaball/ajax/add-name.php
+++ omegaball/ajax/add-name.php
@@ -4,10 +4,20 @@
 
   $conn = connectDB("newOmegaball");
 
-  $profanity = ["fuck", "shit", "bitch", "damn", "cunt", "ass", "whore", "slut"]
-  $length = count($profanity)
+  // Read profanity words from a file and store them in an array.
+  $profanity = [];
+  $file = fopen(realpath($_SERVER["DOCUMENT_ROOT"])."/omegaball/res/profanity.txt", "r");
+  if ($file) {
+      while (($line = fgets($file)) !== false) {
+          $profanity[] = $line;
+      }
+      fclose($file);
+  }
 
-  $name = $_GET["name"];
+  $length = count($profanity);
+
+  // Check if the name contains any profanity.
+  $name = $_POST["name"];
   $name = addslashes($name);
   for($i = 0; $i < $length; $i++){
     if(strcmp($name, $profanity[$i] == 0)){
@@ -16,15 +26,17 @@
     }
   }
 
-  $pos = $_GET["pos"];
-  if(is_numeric($pos) = FALSE){
-    echo "1"
+  // Checks if the value of pos is numeric or not.
+  $pos = $_POST["pos"];
+  if(is_numeric($pos) == FALSE){
+    echo "1";
     exit();
   }
   
   $query = "SELECT playerName FROM PlayerName WHERE playerName = \"$name\"";
   $result = runQuery($conn, $query);
 
+  // Checks the number of rows in the result set and exits if it's greater than 0.
   $num_rows = mysqli_num_rows($result);
   echo $num_rows;
   if($num_rows > 0){
@@ -35,9 +47,9 @@
   $query = "INSERT INTO PlayerName (playerName, position) VALUES (\"$name\", $pos)";
   
 //   $result = runQuery($conn, $query);
-  echo "0"
+  echo "0";
   
-//   while ($row = $result->fetch_assoc()){
+//   while ($row == $result->fetch_assoc()){
 //     $firstName[] = $row["playerName"];
 //   }
 ?>
