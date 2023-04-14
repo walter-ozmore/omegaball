971faaa7d51e3af313eb352401625c36f0bf49bc
Austen Somerville <austensomerville@gmail.com>
2023-04-14 16:44:29 -0500

    Adjusted formatting and added comments.

--- omegaball/suggestions.php
+++ omegaball/suggestions.php
@@ -2,15 +2,21 @@
 <html>
   <head>
     <title>Suggestions</title>
-    <?php require_once realpath($_SERVER["DOCUMENT_ROOT"])."/omegaball/res/head.php"; ?>
+    <?php require_once realpath($_SERVER["DOCUMENT_ROOT"])."/omegaball/res/head.php";?>
     <script>     
+        /**
+         * @brief Generates a Name.
+         *
+         * Generates a first and last name using the information
+         * from the Names SQL Database
+         *
+         */
         function generateNames()
         {
             ajax("/omegaball/ajax/name-gen.php", function(){
             if(this.readyState != 4 || this.status != 200) return;
             let text = this.responseText;
             let obj = JSON.parse(text);
-            console.log(obj);
             let gen = document.getElementById("generate");
             gen.innerHTML = "";
             
@@ -22,6 +28,15 @@
         })
         }
 
+
+        /**
+         * @brief Add a name to the SQL Database
+         *
+         * Allows the users to submit name suggestions to the SQL
+         * Database. Checks if the name is a first name, last name,
+         * both, or neither. If neither, show an error.
+         *
+         */
         function submitName()
         {
             let first = document.getElementById("first");
@@ -67,14 +82,18 @@
             text-align: center;
             align-items: center;
             justify-content: center;
+            padding: 1.5em;
             }
             .errorMessage {
             color: red; /* sets the text color to red */
             }
 
-.error {
-  display: block; /* shows the error message if the input is invalid */
-}
+        .error {
+            display: block; /* shows the error message if the input is invalid */
+        }
+        .words{
+            margin: 1em 0em;
+        }
     </style>
   </head>
 
@@ -90,10 +109,12 @@
         </button>
     </div>
     <div class="box border">
-        <br></br>
+        <p class="words">
+            Please input a name and designate first or last. If both, select both boxes.
+        </p>
         <label for="name">NAME:</label>
         <input type="text" id="name" name="name">
-        <br></br>
+        <p class="words">
         <p>
         <input type="checkbox" name="first" id="first">
         <label for="first">FIRST NAME</label>
@@ -102,8 +123,9 @@
         <input type="checkbox" name="last" id="last">
         <label for="last">LAST NAME</label>
         </p>
+        <p class="words"></p>
         <p class="errorMessage" style="display: none;" id="error"></p>
-        <button onclick="submitName()" pattern="[a-zA-Z]+">
+        <button onclick="submitName()">
             SUBMIT NAME
         </button>
     </div>
