25254a0265fb959613545b76c77bb413457a61de
Walter Ozmore <walter.ozmore.327@gmail.com>
2023-04-24 13:24:10 -0500

    Fixed formating for inline javascript and moved error to global css

--- omegaball/suggestions.php
+++ omegaball/suggestions.php
@@ -4,76 +4,66 @@
     <title>Suggestions</title>
     <?php require_once realpath($_SERVER["DOCUMENT_ROOT"])."/omegaball/res/head.php";?>
     <script>
-        /**
-         * @brief Generates a Name.
-         *
-         * Generates a first and last name using the information
-         * from the Names SQL Database
-         *
-         */
-        function generateNames()
-        {
-            ajax("/omegaball/ajax/name-gen.php", function(){
-            if(this.readyState != 4 || this.status != 200) return;
-            let text = this.responseText;
-            let obj = JSON.parse(text);
-            let gen = document.getElementById("generate");
-            gen.innerHTML = "";
+      /**
+       * @brief Generates a Name.
+       *
+       * Generates a first and last name using the information
+       * from the Names SQL Database
+       *
+       */
+      function generateNames() {
+        ajax("/omegaball/ajax/name-gen.php", function() {
+          if(this.readyState != 4 || this.status != 200) return;
+          let text = this.responseText;
+          let obj = JSON.parse(text);
+          let gen = document.getElementById("generate");
+          gen.innerHTML = "";
 
-            for(let name of obj)
-            {
-                gen.appendChild(mkEle("p", name));
-            }
+          for(let name of obj) {
+              gen.appendChild(mkEle("p", name));
+          }
+        });
+      }
 
-        })
+
+      /**
+       * @brief Add a name to the SQL Database
+       *
+       * Allows the users to submit name suggestions to the SQL
+       * Database. Checks if the name is a first name, last name,
+       * both, or neither. If neither, show an error.
+       *
+       */
+      function submitName() {
+        let first = document.getElementById("first");
+        let last = document.getElementById("last");
+        let pos = -1;
+        let name = document.getElementById("name").value;
+
+        if (first.checked && last.checked) {
+          console.log("This is both a first and last name");
+          pos = -1;
+        } else if(last.checked) {
+          console.log("This is a last name");
+          pos = 3;
+        } else if(first.checked) {
+          console.log("This is a first name");
+          pos = 1;
+        } else {
+          console.log("Please select if the name is first, last, or both");
+          let error = document.getElementById("error");
+          error.innerHTML = "PLEASE SELECT FIRST, LAST, OR BOTH.";
+          error.style.display = "block";
         }
 
+        let args ="pos="+pos+"&name="+name;
 
-        /**
-         * @brief Add a name to the SQL Database
-         *
-         * Allows the users to submit name suggestions to the SQL
-         * Database. Checks if the name is a first name, last name,
-         * both, or neither. If neither, show an error.
-         *
-         */
-        function submitName()
-        {
-            let first = document.getElementById("first");
-            let last = document.getElementById("last");
-            let pos = -1;
-            let name = document.getElementById("name").value;
-            console.log(name);
-
-            if (first.checked && last.checked) {
-                console.log("This is both a first and last name");
-                pos = -1;
-            }
-            else if(last.checked){
-                console.log("This is a last name");
-                pos = 3;
-            }
-            else if(first.checked){
-                console.log("This is a first name");
-                pos = 1;
-            }
-            else{
-                console.log("Please select if the name is first, last, or both");
-                let error = document.getElementById("error");
-                error.innerHTML = "PLEASE SELECT FIRST, LAST, OR BOTH.";
-                error.style.display = "block";
-            }
-
-
-
-            let args ="pos="+pos+"&name="+name;
-
-            ajax("/omegaball/ajax/add-name.php", function(){
-                if(this.readyState != 4 || this.status != 200) return;
-                let text = this.responseText;
-                console.log(text);
-            }, args);
-        }
+        ajax("/omegaball/ajax/add-name.php", function() {
+          if(this.readyState != 4 || this.status != 200) return;
+          let text = this.responseText;
+          console.log(text);
+        }, args);
+      }
     </script>
     <style>
       .box {
@@ -85,13 +75,6 @@
         padding: 1.5em;
       }
 
-      .errorMessage {
-        color: red; /* sets the text color to red */
-      }
-
-      .error {
-        display: block; /* shows the error message if the input is invalid */
-      }
       .words {
         margin: 0em 0em 1em 0em;
       }
@@ -120,7 +103,7 @@
         <label for="last">LAST NAME</label>
       </p>
 
-      <p class="errorMessage" style="display: none;" id="error"></p>
+      <p id="error" class="error" style="display: none;"></p>
       <button onclick="submitName()">SUBMIT NAME</button>
     </div>
 
